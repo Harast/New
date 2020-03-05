f5c0661ba09404f7d51aba44a653ca27edd0e6a9
Harast <Harastey@gmail.com>
2020-03-05 18:15:39 +0200

    JS

--- JS.js
+++ JS.js
@@ -1,5 +1,11 @@
+var str = '';
 function isright(obj)
 {
 if (obj.value>1e6) obj.value='err'; 
 if (obj.value<1) obj.value='';
+str = str + obj.value
+console.log(str);
+}
+function count(){
+	alert(str);
 }
