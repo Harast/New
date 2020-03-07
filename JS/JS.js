0c39a97fd6577838024ece9f20df2262eacf3f62
Harast <Harastey@gmail.com>
2020-03-07 17:06:58 +0200

    JS

--- JS/JS.js
+++ JS/JS.js
@@ -1,5 +1,4 @@
-var str = '';
-var r =[];
+var str = 0;
 function isright(obj)
 {
 if (obj.value>1e6) obj.value='err'; 
@@ -8,9 +7,11 @@ str = obj.value
 console.log(str);
 }
 function count(){
-	for (var str1 = str-1; str1>=1; str1 = str1-1);{
-	r [str1] = str1
-	str = str + ',' + r;
-	console.log(str);
-	alert(r);
+var arr =[str-1];
+	for (var i = str-1; i>=1; i--){
+	arr [i] = str - i;
+	//str = str + arr;
+	console.log(arr);
+	document.getElementById("arr").innerHTML = arr;
+	//alert(arr);
 }}
