04bd93f1eff2e32cedb6497a81f5ceb8c7e8e203
Harast <Harastey@gmail.com>
2020-03-11 16:29:10 +0200

    JS

--- JS/JS.js
+++ JS/JS.js
@@ -1,20 +1,28 @@
 var str = 0;
+var j = 0;
+var j1 = 0;
+var arr1 =[];
+
 function isright(obj)
 {
 if (obj.value>1e6) obj.value='err'; 
 if (obj.value<1) obj.value='';
-str = obj.value
+str = obj.value;
+j=Math.trunc(Math.sqrt(str));
+console.log(j);
 console.log(str);
 }
 function count(){
-var arr =[];
-var r=str 
-	for (var i = 1; i<str; i++){
-	if (r%3 !== 0 || r==3){
-		if(r%2 !== 0 || r==2){
-	arr [i] = r;}}
-	r = r -1;
-	console.log(arr);
-	document.getElementById("arr").innerHTML = arr;
-	//alert(arr);
-}}
+var j1 = j;
+var ar='';
+	for (var i = 0; i<j; i++){
+	if (j1>1 && j1%2 !== 0 || j1==2){
+		if(j1%3 !== 0 || j1==3){
+	arr1 [i] = j1;}}
+	j1 = j1 -1;}
+for (ji = 0; ji <j; ji++) {
+  if (arr1[ji] !== undefined){
+    ar = ar +','+arr1[ji];}}
+	console.log(ar);
+	document.getElementById("ar").innerHTML = ar;
+	}
