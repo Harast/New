add1566b9c1b9eb27ce9386354f2cabfd6248d2e
Harast <Harastey@gmail.com>
2020-03-11 17:37:07 +0200

    JS

--- JS/JS.js
+++ JS/JS.js
@@ -25,4 +25,28 @@ for (ji = 0; ji <j; ji++) {
     ar = ar +','+arr1[ji];}}
 	console.log(ar);
 	document.getElementById("ar").innerHTML = ar;
-	}
+}
+function Fizz_Buzz()
+{	
+var j1=str
+	for (var i = 0; i<str; i++){
+	if (j1%3 == 0)
+    {if(j1%5 == 0)
+		{arr1 [i] = 'FizzBuzz';}
+		else{
+	arr1 [i] = 'Fizz';}}
+      else{
+		if(j1%5 == 0)
+        {arr1 [i] = 'Buzz';}
+      else{
+	  arr1 [i] = j1;}}
+	j1 = j1 -1;}
+/*for (ji = 0; ji <j; ji++) {
+  if (arr1[ji] !== undefined){
+    ar = ar +','+arr1[ji];}}
+	console.log(ar);*/
+    console.log(arr1);
+	document.getElementById("arr1").innerHTML = arr1;
+}
+
+	
