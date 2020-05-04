c01da620bf09ccc0c3b23b14a03f5f353e29506f
Harast <Harastey@gmail.com>
2020-05-04 13:35:43 +0300

    Update radio button

--- JS/JS.js
+++ JS/JS.js
@@ -79,3 +79,12 @@ Pal = '';
   }
  document.getElementById("Pal").innerHTML = Pal;
 }
+
+function fun1() {
+    var rad=document.getElementsByName('r1');
+    for (var i=0;i<rad.length; i++) {
+        if (rad[i].checked) {
+            alert('Выбран '+i+' radiobutton');
+        }
+    }
+}
